ffe41c48dd8d8f8b82ea7fa653d5edc0dd54cfe3
Maximilian Scheurer <m.scheurer@stud.uni-heidelberg.de>
2022-03-03 21:46:56 +0100

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,6 @@
 # don't run the tests for Python 3.10 on macOS
 ver=$(python -c 'import sys; print(sys.version_info.minor)')
-if [ $ver -eq 9 ] && [ $(uname) == "Darwin" ]; then
+if [ $ver -eq 10 ] && [ $(uname) == "Darwin" ]; then
     exit 0
 fi
 # install pyscf
