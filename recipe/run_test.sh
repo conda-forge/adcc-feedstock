347aa29d19efc8f962ee50993f5e34d550323add
Lori A. Burns <lori.burns@gmail.com>
2023-02-13 16:03:12 -0500

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,6 @@
 # don't run the tests for Python 3.10 (pyscf not working)
 ver=$(python -c 'import sys; print(sys.version_info.minor)')
-if [ $ver -eq 10 ]; then
+if [ $ver -gt 10 ]; then
     exit 0
 fi
 # install pyscf
