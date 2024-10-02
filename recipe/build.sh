848411a68772684c49a71087e4c305e51b26d308
Lori A. Burns <lori.burns@gmail.com>
2024-10-02 15:50:06 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
+if [[ "$target_platform" == "linux-ppc64le" ]]; then
+  CFLAGS="$(echo $CFLAGS | sed 's/-fno-plt //g')"
+  CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fno-plt //g')"
+  LDFLAGS="$(echo $LDFLAGS | sed 's/-fno-plt //g')"
+fi
+
 ./scripts/remove_test_files.sh
 # ${PYTHON} setup.py install --prefix=${PREFIX}
 ${PYTHON} -m pip install . -vv --no-deps
