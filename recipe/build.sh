ceb6275f07f76b6d94580fec11ded105ab3690a3
Maximilian Scheurer <max.scheurer@me.com>
2024-10-15 11:37:06 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
-nm $PREFIX/lib/libtensorlight$SHLIB_EXT | grep product_table_container | grep get_instance
-nm $PREFIX/lib/libtensorlight$SHLIB_EXT | grep product_table_container | grep ret_table
+# nm $PREFIX/lib/libtensorlight$SHLIB_EXT | grep product_table_container | grep get_instance
+# nm $PREFIX/lib/libtensorlight$SHLIB_EXT | grep product_table_container | grep ret_table
+
+CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fvisibility-inlines-hidden //g')"
 
 ./scripts/remove_test_files.sh
 # ${PYTHON} setup.py install --prefix=${PREFIX}
